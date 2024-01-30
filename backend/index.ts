6dd67b7b7973fed5787084206e7b3bda0967e639
Ege <ege.bilge@codana.de>
2024-01-30 16:18:33 +0300

    fix: small fix #1

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import blogRoutes from "./src/routes/blogRoutes/index";
+import blogRoutes from "./src/routes/blogRoutes";
 
 dotenv.config();
 
